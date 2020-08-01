cbe162a920086e0fc8061735679d6ac742ded205
Naomi Stuart <nstu778@aucklanduni.ac.nz>
2020-08-01 23:31:49 +1000

    Update project 0b - tic-tac-toe

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,35 +23,105 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+
+    # If board is empty, X gets the first move
+    if board == initial_state():
+        return X
+
+    # Count number of X's and O's on board
+    numX = 0
+    numO = 0
+    for row in board:
+        numX += row.count(X)
+        numO += row.count(O)
+
+    # Next player is the one with the fewest moves currently
+    if numX < numO:
+        return X
+    else:
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
 
+    # (row, cell) is legal move if its position on the board is currently empty
+    legal_actions = set()
+    for row in range(3):
+        for cell in range(3):
+            if board[row][cell] == EMPTY:
+                legal_actions.add((row, cell))
+
+    return legal_actions
+    
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+
+    # Raise exception if index out of range or cell is already taken
+    if action[0] not in range(0, 3) or action[1] not in range(0, 3) or board[action[0]][action[1]] is not EMPTY:
+        raise Exception("Invalid move")
+
+    # Let player whose turn it is to make their move on the board
+    new_board = copy.deepcopy(board)
+    new_board[action[0]][action[1]] = player(board)
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    
+    # Evaluate board for each player
+    for mark in [X, O]:
+        
+        # Check for 3 moves in a row horizontally
+        for row in range(0, 3):
+            if all(board[row][col]==mark for col in range(0, 3)):
+                return mark
+        
+        # Check for 3 moves in a row vertically
+        for col in range(0, 3):
+            if all(board[row][col]==mark for row in range(0, 3)):
+                return mark
 
+        # Check for 3 moves in a row diagonally
+        diagonals = [[(0, 0), (1, 1), (2, 2)], [(0, 2), (1, 1), (2, 0)]]
+        for diagonal in diagonals:
+            if all(board[row][col]==mark for (row, col) in diagonal):
+                return mark
 
+    # Game is a tie or still in progress
+    return None
+
+        
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    
+    # Terminal if there is a winner
+    if winner(board) is not None:
+        return True
+    
+    # Terminal if all cells have been filled
+    all_moves = [cell for row in board for cell in row]
+    if not any(move==EMPTY for move in all_moves):
+        return True
+
+    # num_filled = 0
+    # for row in board:
+    #     num_filled += row.count(X) + row.count(O)
+    # if num_filled == 9:
+    #     return True
+
+    # Otherwise game in progress
+    return False
 
 
 def utility(board):
